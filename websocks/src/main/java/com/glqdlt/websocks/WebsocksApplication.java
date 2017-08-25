2bf832512d1d15009462ee183f6337ca43d6116d
iw.jhun <glqdlt@naver.com>
2017-08-25 14:05:12 +0900

    레퍼런스 경로 추가

--- websocks/src/main/java/com/glqdlt/websocks/WebsocksApplication.java
+++ websocks/src/main/java/com/glqdlt/websocks/WebsocksApplication.java
@@ -3,6 +3,8 @@ package com.glqdlt.websocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//https://spring.io/guides/gs/messaging-stomp-websocket/
 @SpringBootApplication
 public class WebsocksApplication {
 
